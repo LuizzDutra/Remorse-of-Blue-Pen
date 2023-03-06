ad5634d50fa4b8ad2a7971b9c0c130de5c24abb8
Luiz <luizzidutra@gmail.com>
2023-03-06 19:08:49 -0300

    player sprites test added and camera zoom adjustment

--- Scripts/Gun.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _physics_process(_delta):
-	rotation = get_global_mouse_position().angle_to_point(global_position)
-	if rotation_degrees > 90 or rotation_degrees < -90:
-		$Arm.rect_scale.y = -1
-	else:
-		$Arm.rect_scale.y = 1
