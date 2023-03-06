4e69cd66cbac293be04c163c96a45335fe385c29
Luiz <luizzidutra@gmail.com>
2023-03-06 14:25:29 -0300

    in-game menu keyboard control and menu pause

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,33 +2,37 @@ extends Node
 
 var hub_scene = load("res://Scenes/Levels/Hub.tscn")
 
-onready var menu = $CanvasLayer/Menu
+onready var menu = $CanvasLayer/GameMenu
 
 func _on_Hub_level_entered(loaded_level):
 	$Hub.queue_free()
 	add_child(loaded_level.instance())
 
-
 func _unhandled_input(event):
 	if event.is_action("ui_cancel"):
 		if event.is_pressed():
 			if menu.visible:
-				menu.visible = false
+				get_tree().paused = false
+				menu.set_visibility(false, true)
 			elif not menu.visible:
-				menu.visible = true
+				get_tree().paused = true
 				if has_node("Hub"):
-					menu.get_node("GridContainer/BackToHub").visible = false
+					menu.set_visibility(true, false)
 				else:
-					menu.get_node("GridContainer/BackToHub").visible = true
+					menu.set_visibility(true, true)
+				
 
 
 func _on_BackToMenu_pressed():
+	get_tree().paused = false
 	get_tree().change_scene("res://Base.tscn")
 
 
 func _on_BackToHub_pressed():
+	get_tree().paused = false
 	get_tree().change_scene("res://Scenes/Game.tscn")
 
 
 func _on_Return_pressed():
 	menu.visible = false
+	get_tree().paused = false
