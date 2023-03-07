13282078f47115254cf931ed12cd7455e60ddc0c
Luiz <luizzidutra@gmail.com>
2023-03-07 11:39:59 -0300

    new menu sounds

--- Scripts/autoloads/ButtonSound.gd
+++ Scripts/autoloads/ButtonSound.gd
@@ -1,7 +1,7 @@
 extends AudioStreamPlayer
 
-var press_sound = load("res://Assets/Sounds/buttontest.wav")
-var hover_sound = load("res://Assets/Sounds/buttonhovertest.wav")
+var press_sound = load("res://Assets/Sounds/select.mp3")
+var hover_sound = load("res://Assets/Sounds/preselect.mp3")
 
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
